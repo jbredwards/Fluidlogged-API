93fc7c51751d02805d21fbd1776c41eb93f7d47d
jbredwards <jbredwards@gmail.com>
2021-05-27 11:07:42 -0700

    5/27/2021
    -actually fixed SpongeForge incompatibility this time (issue #1).
    -the BlockFluidloggedTE class now updates when the stored state is changed when right clicked.

--- src/main/java/git/jbredwards/fluidlogged/util/FluidloggedConstants.java
+++ src/main/java/git/jbredwards/fluidlogged/util/FluidloggedConstants.java
@@ -17,7 +17,7 @@ public enum FluidloggedConstants
 
     public static final String MODID = "fluidlogged_api";
     public static final String NAME = "Fluidlogged API";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
 
     //used to get the fluidlogged te's from the fluid
     public static final Map<Fluid, BlockFluidloggedTE> FLUIDLOGGED_TE_LOOKUP = new HashMap<>();
