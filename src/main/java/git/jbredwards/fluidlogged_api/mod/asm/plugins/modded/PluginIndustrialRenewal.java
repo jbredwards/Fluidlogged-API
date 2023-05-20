9a0e6925aff9604791beeab5029065a65381e329
jbredwards <jbredwards@gmail.com>
2023-05-20 13:21:38 -0400

    release v2.2.2
    Bug Fixes:
    -fix PluginIndustrialRenewal crash

--- src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/PluginIndustrialRenewal.java
+++ src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/PluginIndustrialRenewal.java
@@ -1,6 +1,7 @@
 package git.jbredwards.fluidlogged_api.mod.asm.plugins.modded;
 
 import git.jbredwards.fluidlogged_api.api.asm.IASMPlugin;
+import net.minecraft.block.Block;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import org.objectweb.asm.tree.*;
@@ -19,9 +20,17 @@ public final class PluginIndustrialRenewal implements IASMPlugin
 
     @Override
     public boolean transform(@Nonnull InsnList instructions, @Nonnull MethodNode method, @Nonnull AbstractInsnNode insn, boolean obfuscated, int index) {
+        /*
+         * canPlaceBlockAt:
+         * Old code:
+         * this.isReplaceable(worldIn, pos)
+         *
+         * New code:
+         * //fix industrial renewal mod's multi-blocks
+         * Hooks.isReplaceable(this, worldIn, pos)
+         */
         if(checkMethod(insn, obfuscated ? "func_176200_f" : "isReplaceable")) {
-            instructions.insert(insn, genMethodNode("isReplaceable", "(Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;)Z"));
-            instructions.remove(getPrevious(insn, 3));
+            instructions.insert(insn, genMethodNode("isReplaceable", "(Lnet/minecraft/block/Block;Lnet/minecraft/world/IBlockAccess;Lnet/minecraft/util/math/BlockPos;)Z"));
             instructions.remove(insn);
         }
 
@@ -31,7 +40,7 @@ public final class PluginIndustrialRenewal implements IASMPlugin
     @SuppressWarnings("unused")
     public static final class Hooks
     {
-        public static boolean isReplaceable(@Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
+        public static boolean isReplaceable(@Nonnull Block unused, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
             return world.getBlockState(pos).getBlock().isReplaceable(world, pos);
         }
     }
