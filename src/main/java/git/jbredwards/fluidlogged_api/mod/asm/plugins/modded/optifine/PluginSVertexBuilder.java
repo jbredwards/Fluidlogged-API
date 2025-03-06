7fbc1254baf8db91243a09fb770957d61963d7b0
jbredwards <jbredwards@gmail.com>
2025-03-06 10:54:48 -0500

    Release v3.0.3
    **Bug Fixes:**
    - Fix crash with Fugue + Optifine ([issue#232](https://github.com/jbredwards/Fluidlogged-API/issues/232))

--- src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/optifine/PluginSVertexBuilder.java
+++ src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/optifine/PluginSVertexBuilder.java
@@ -6,8 +6,8 @@
 package git.jbredwards.fluidlogged_api.mod.asm.plugins.modded.optifine;
 
 import git.jbredwards.fluidlogged_api.api.asm.IASMPlugin;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumBlockRenderType;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.InsnList;
@@ -35,11 +35,11 @@ public final class PluginSVertexBuilder implements IASMPlugin
          *
          * New code:
          * // tell Optifine to pass the old render type for BlockLiquid blocks into shaders
-         * int renderType = Hooks.getRenderType(block.getRenderType(blockState), block).ordinal();
+         * int renderType = Hooks.getRenderType(block.getRenderType(blockState), blockState).ordinal();
          */
         if(checkMethod(insn, obfuscated ? "func_149645_b" : "getRenderType")) {
-            instructions.insert(insn, genMethodNode("getRenderType", "(Lnet/minecraft/util/EnumBlockRenderType;Lnet/minecraft/block/Block;)Lnet/minecraft/util/EnumBlockRenderType;"));
-            instructions.insert(insn, new VarInsnNode(ALOAD, 4));
+            instructions.insert(insn, genMethodNode("getRenderType", "(Lnet/minecraft/util/EnumBlockRenderType;Lnet/minecraft/block/state/IBlockState;)Lnet/minecraft/util/EnumBlockRenderType;"));
+            instructions.insert(insn, new VarInsnNode(ALOAD, 0));
             return true;
         }
 
@@ -50,8 +50,8 @@ public final class PluginSVertexBuilder implements IASMPlugin
     public static final class Hooks
     {
         @Nonnull
-        public static EnumBlockRenderType getRenderType(@Nonnull final EnumBlockRenderType oldType, @Nonnull final Block block) {
-            return block instanceof BlockLiquid ? EnumBlockRenderType.LIQUID : oldType;
+        public static EnumBlockRenderType getRenderType(@Nonnull final EnumBlockRenderType oldType, @Nonnull final IBlockState state) {
+            return state.getBlock() instanceof BlockLiquid ? EnumBlockRenderType.LIQUID : oldType;
         }
     }
 }
