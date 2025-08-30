e1788c45e5c60681f0b864f85f20150b034398fc
jbredwards <jbredwards@gmail.com>
2025-08-30 15:09:15 -0400

    Support old Galacticraft
    - Fixes #255.

--- src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/galacticraft/PluginGCBlocks.java
+++ src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/galacticraft/PluginGCBlocks.java
@@ -7,6 +7,7 @@ package git.jbredwards.fluidlogged_api.mod.asm.plugins.modded.galacticraft;
 
 import git.jbredwards.fluidlogged_api.api.asm.IASMPlugin;
 import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.InsnList;
 import org.objectweb.asm.tree.MethodNode;
 
@@ -19,6 +20,8 @@ import javax.annotation.Nonnull;
  */
 public final class PluginGCBlocks implements IASMPlugin
 {
+    private int toRemove;
+
     @Override
     public boolean isMethodValid(@Nonnull final MethodNode method, final boolean obfuscated) { return method.name.equals("registerBlocks"); }
 
@@ -35,12 +38,18 @@ public final class PluginGCBlocks implements IASMPlugin
          * ...
          * ...
          */
-        if(checkField(insn, "gratingWater")) removeFrom(instructions, insn, 2);
+        if(checkField(insn, "gratingWater")) removeFrom(instructions, insn, toRemove);
         else if(checkField(insn, "gratingLava")) {
-            removeFrom(instructions, insn, 2);
+            removeFrom(instructions, insn, toRemove);
             return true;
         }
 
         return false;
+    }
+
+    @Override
+    public boolean transformClass(@Nonnull final ClassNode classNode, final boolean obfuscated) {
+        toRemove = classNode.methods.stream().anyMatch(method -> checkMethod(method, "registerBlock", "(Lnet/minecraft/block/Block;Ljava/lang/Class;)V")) ? 2 : 4;
+        return true;
     }
 }
