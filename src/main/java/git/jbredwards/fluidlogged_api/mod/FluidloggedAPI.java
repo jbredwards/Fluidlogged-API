e3a36992007d7ad650af0e2cebbeff5855f1e9f1
jbredwards <jbredwards@gmail.com>
2026-02-05 16:50:08 -0500

    Release v3.2.0
    **Features:**
    - Add Modrinth `updateJSON` url.
    - Minor performance improvements.
    
    **Bug Fixes:**
    - Fix concurrency issue with `FluidFlowHandler::getFlowVec` (fixes [#275](https://github.com/jbredwards/Fluidlogged-API/issues/275) and [#283](https://github.com/jbredwards/Fluidlogged-API/issues/283)).
    - Fix crash with Java 9+ (fixes [#284](https://github.com/jbredwards/Fluidlogged-API/issues/284)) (this bug only affected v3.1.3 of Fluidlogged API).
    - Fix minor inconsistency with normal fluid flow logic.
    
    **Compatibility Fixes:**
    - Gauges and Switches:
      - Fix blocks not emitting a redstone signal (fixes [#282](https://github.com/jbredwards/Fluidlogged-API/issues/282)).
    - Schematica:
      - Fix crash when rendering schematica with fluids (fixes [#279](https://github.com/jbredwards/Fluidlogged-API/issues/279)).
    
    **API Changes:**
    - New method: `...api.util.IFluidloggable.canFluidConnect(IBlockAccess, BlockPos, IBlockState, EnumFacing)`.
    - New method: `...api.util.FluidloggedUtils.canFluidConnect(IBlockAccess, BlockPos, IBlockState, EnumFacing)`.
    - Restore `FluidloggedUtils` methods that were accidentally removed in v3.0.0.

--- src/main/java/git/jbredwards/fluidlogged_api/mod/FluidloggedAPI.java
+++ src/main/java/git/jbredwards/fluidlogged_api/mod/FluidloggedAPI.java
@@ -65,7 +65,7 @@ import java.util.Optional;
  * @author jbred
  *
  */
-@Mod(modid = FluidloggedAPI.MODID, name = "Fluidlogged API", version = "3.1.3",
+@Mod(modid = FluidloggedAPI.MODID, name = "Fluidlogged API", version = "3.2.0",
      updateJSON = "https://api.modrinth.com/updates/fluidlogged-api/forge_updates.json",
      guiFactory = "git.jbredwards.fluidlogged_api.mod.client.config.gui.FluidloggedAPIGuiFactory")
 public final class FluidloggedAPI
