1e5db8eba3a3c015c0bf5a702944ea29d47bd341
jbredwards <jbredwards@gmail.com>
2026-03-27 15:21:31 -0400

    Fix #291

--- src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/galacticraft/PluginGalacticraft.java
+++ src/main/java/git/jbredwards/fluidlogged_api/mod/asm/plugins/modded/galacticraft/PluginGalacticraft.java
@@ -18,6 +18,7 @@ package git.jbredwards.fluidlogged_api.mod.asm.plugins.modded.galacticraft;
 
 import git.jbredwards.fluidlogged_api.api.asm.IASMPlugin;
 import git.jbredwards.fluidlogged_api.api.util.FluidloggedUtils;
+import git.jbredwards.fluidlogged_api.api.world.IWorldProvider;
 import git.jbredwards.fluidlogged_api.mod.common.fluid.handler.FluidCollisionHandler;
 import micdoodle8.mods.galacticraft.core.proxy.ClientProxyCore;
 import net.minecraft.block.material.Material;
@@ -26,10 +27,11 @@ import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.*;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -43,6 +45,28 @@ public final class PluginGalacticraft implements IASMPlugin
 {
     public final boolean isFluidUtil;
     public PluginGalacticraft(boolean isFluidUtilIn) { isFluidUtil = isFluidUtilIn; }
+
+    @Override
+    public boolean isMethodValid(@Nonnull final MethodNode method, final boolean obfuscated) { return method.name.equals("isFlammable"); }
+
+    @Override
+    public boolean transform(@Nonnull final InsnList instructions, @Nonnull final MethodNode method, @Nonnull final AbstractInsnNode insn, final boolean obfuscated, final int index) {
+        /*
+         * Old code:
+         * ((World)world).createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 6.0F, true);
+         *
+         * New code:
+         * // fix ClassCastException when fire interacts with Galacticraft fluids
+         * Hooks.getWorld(world).createExplosion(null, pos.getX(), pos.getY(), pos.getZ(), 6.0F, true);
+         */
+        if(insn.getOpcode() == CHECKCAST && ((TypeInsnNode)insn).desc.equals("net/minecraft/world/World")) {
+            method.instructions.insert(insn, genMethodNode("getWorld", "(Lnet/minecraft/world/IBlockAccess;)Lnet/minecraft/world/World;"));
+            method.instructions.remove(insn);
+            return true;
+        }
+
+        return false;
+    }
 
     @Override
     public boolean transformClass(@Nonnull ClassNode classNode, boolean obfuscated) {
@@ -60,6 +84,7 @@ public final class PluginGalacticraft implements IASMPlugin
                 generator.visitVarInsn(ALOAD, 0);
                 generator.visitVarInsn(ALOAD, 1);
             });
+            return false;
         }
 
         else {
@@ -75,14 +100,19 @@ public final class PluginGalacticraft implements IASMPlugin
                     generator.visitVarInsn(ILOAD, 8);
                 }
             );
+            return true;
         }
-
-        return false;
     }
 
     @SuppressWarnings("unused")
     public static final class Hooks
     {
+        @Nonnull
+        public static World getWorld(@Nonnull final IBlockAccess access) {
+            // Modern java versions can have issues with ASM referring to an interface static method directly.
+            return IWorldProvider.getWorld(access);
+        }
+
         @Nullable
         public static Boolean isEntityInsideMaterial(@Nonnull final IBlockAccess access, @Nonnull final BlockPos pos, @Nonnull final IBlockState state, @Nonnull final Entity entity, final double yToTest, @Nonnull final Material material, final boolean testingHead) {
             return FluidCollisionHandler.isEntityInsideMaterial(access, pos, state, entity, yToTest, material == Material.WATER ? state.getMaterial() : material, testingHead);
